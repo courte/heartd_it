7173a4008c5cbc4b1eb292a22f97af0bbe59777f
Courteney Ervin <courteney.ervin@gmail.com>
2014-11-20 11:18:44 -0500

    Add individual sample tweet for development

---
+++ app/js/models/sample-tweet.js
@@ -0,0 +1,101 @@
+SAMPLE_TWEET = [
+  { "created_at":"Wed Nov 12 15:10:23 +0000 2014",
+    "id":532550983776100352,
+    "id_str":"532550983776100352",
+    "text":"Exciting Jen News: I'm happy to announce that, starting next Monday, I will be the Director of Open Source Curriculum at @pluralsight.",
+    "source":"\u003ca href=\"https:\/\/about.twitter.com\/products\/tweetdeck\" rel=\"nofollow\"\u003eTweetDeck\u003c\/a\u003e",
+    "truncated":false,
+    "in_reply_to_status_id":null,
+    "in_reply_to_status_id_str":null,
+    "in_reply_to_user_id":null,
+    "in_reply_to_user_id_str":null,
+    "in_reply_to_screen_name":null,
+    "user":
+      { "id":588743,
+        "id_str":"588743",
+        "name":"Jen Myers",
+        "screen_name":"antiheroine",
+        "location":"Chicago",
+        "profile_location":null,
+        "description":"Web designer\/developer, speaker, troublemaker. Advisor\/teacher at @girldevelopit. Founder of @codecupcakeschi. Has a nifty newsletter: http:\/\/t.co\/i4baKxA5KC",
+        "url":"http:\/\/t.co\/wCmIbjRQrO",
+        "entities":
+          { "url":
+            { "urls":
+              [
+                { "url":"http:\/\/t.co\/wCmIbjRQrO",
+                  "expanded_url":"http:\/\/jenmyers.net",
+                  "display_url":"jenmyers.net",
+                  "indices":[0,22]
+                }
+              ]
+            },
+            "description":
+            { "urls":
+              [
+                { "url":"http:\/\/t.co\/i4baKxA5KC",
+                  "expanded_url":"http:\/\/tinyletter.com\/jenmyers",
+                  "display_url":"tinyletter.com\/jenmyers",
+                  "indices":[135,157]
+                }
+              ]
+            }
+          },
+        "protected":false,
+        "followers_count":12673,
+        "friends_count":1383,
+        "listed_count":808,
+        "created_at":"Fri Jan 05 17:24:07 +0000 2007",
+        "favourites_count":1282,
+        "utc_offset":-21600,
+        "time_zone":"Central Time (US & Canada)",
+        "geo_enabled":true,
+        "verified":false,
+        "statuses_count":25827,
+        "lang":"en",
+        "contributors_enabled":false,
+        "is_translator":false,
+        "is_translation_enabled":false,
+        "profile_background_color":"E4EAE9",
+        "profile_background_image_url":"http:\/\/pbs.twimg.com\/profile_background_images\/378800000072353436\/6d4bed3e54202a6a5ca8dbd548dbf006.jpeg",
+        "profile_background_image_url_https":"https:\/\/pbs.twimg.com\/profile_background_images\/378800000072353436\/6d4bed3e54202a6a5ca8dbd548dbf006.jpeg",
+        "profile_background_tile":false,
+        "profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/528165243961102337\/6SEpv7ld_normal.png",
+        "profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/528165243961102337\/6SEpv7ld_normal.png",
+        "profile_banner_url":"https:\/\/pbs.twimg.com\/profile_banners\/588743\/1413048604",
+        "profile_link_color":"39AA9F",
+        "profile_sidebar_border_color":"FFFFFF",
+        "profile_sidebar_fill_color":"EFEFEF",
+        "profile_text_color":"333333",
+        "profile_use_background_image":false,
+        "default_profile":false,
+        "default_profile_image":false,
+        "following":true,
+        "follow_request_sent":false,
+        "notifications":false
+      },
+    "geo":null,
+    "coordinates":null,
+    "place":null,
+    "contributors":null,
+    "retweet_count":13,
+    "favorite_count":86,
+    "entities":
+      { "hashtags":[],
+        "symbols":[],
+        "user_mentions":
+          [
+            { "screen_name":"pluralsight",
+              "name":"Pluralsight ",
+              "id":19253334,
+              "id_str":"19253334",
+              "indices":[121,133]
+            }
+          ],
+        "urls":[]
+      },
+    "favorited":true,
+    "retweeted":false,
+    "lang":"en"
+  }
+];
