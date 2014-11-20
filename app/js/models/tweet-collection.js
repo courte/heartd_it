b2cf976ea939e756248283fbb9623cad09168c72
Courteney Ervin <courteney.ervin@gmail.com>
2014-11-20 17:52:14 -0500

    Rename TweetCollection file

--- app/js/models/tweet-collection.js
+++ app/js/models/tweet-collection.js
@@ -4,8 +4,10 @@ function TweetCollection() {
 }
 
 TweetCollection.prototype.fetch = function() {
+  this.models = [];
+
   _.map(SAMPLE_TWEETS, function(tweet_data) {
-    this.models.push(new Tweet(tweet_data));
+    this.models.push(new this.model(tweet_data));
   }, this);
 
   return this.models;
