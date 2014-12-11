3bf9cea2467161768074e5c2a663f0f5a525fd32
Courteney Ervin <courteney.ervin@gmail.com>
2014-12-11 15:54:03 -0500

    Add keyup event on search field to filter tweets

--- app/js/views/search-view.js
+++ app/js/views/search-view.js
@@ -1,3 +1,10 @@
-function SearchView(){
+function SearchView(collection) {
+  this.collection = collection;
   this.$el = $('#search');
+
+  this.$el.on('keyup', '#search-text', function() {
+    event.preventDefault();
+    var textInput = $('#search-text').val();
+    App.displayTweetList(this.collection.filterContentBy(textInput));
+  }.bind(this));
 }
