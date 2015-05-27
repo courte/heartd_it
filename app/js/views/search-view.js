28b5f53346871eb76298755d2d807fea5fe2092b
Courteney Ervin <courteney.ervin@gmail.com>
2015-05-27 19:02:46 -0400

    Remove unnecessary event default prevention

--- app/js/views/search-view.js
+++ app/js/views/search-view.js
@@ -3,7 +3,6 @@ function SearchView(collection) {
   this.$el = $('#search');
 
   this.$el.on('keyup', '#search-text', function() {
-    event.preventDefault();
     var textInput = $('#search-text').val();
     App.displayTweetList(this.collection.filterContentBy(textInput));
   }.bind(this));
